5fa2379c6b2cb98e031924ecb85a108e62db02e8
Vincent Vargas <vincentvargas6969@gmail.com>
2023-02-05 05:30:56 -0800

    displaying the users

--- website/models.py
+++ website/models.py
@@ -10,7 +10,9 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True, nullable = False)
     password = db.Column(db.String(150), nullable = False)
 
-
+    def _init__(self,name,email):
+        self.name = name
+        self.email = email
 
 
 
--- website/views.py
+++ website/views.py
@@ -1,10 +1,13 @@
 from flask import Blueprint, flash, render_template
 from flask_login import current_user, login_required
+from .models import User
 
 views= Blueprint('views', __name__)
 
-@views.route('/')
+@views.route('/', methods =['GET', 'POST'])
 @login_required
 def home():
-    return render_template('home.html')
+    
+    user = User.query.all()
+    return render_template('home.html',user=user)
 
