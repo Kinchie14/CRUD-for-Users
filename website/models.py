2e49383675391a2294f7928d7c1909a410c20f24
Vincent Vargas <vincentvargas6969@gmail.com>
2023-02-09 00:45:20 -0800

    asd

--- website/models.py
+++ website/models.py
@@ -10,7 +10,7 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True, nullable = False)
     password = db.Column(db.String(150), nullable = False)
 
-    def _init__(self,name,email):
+    def __init__(self,name,email):
         self.name = name
         self.email = email
 
